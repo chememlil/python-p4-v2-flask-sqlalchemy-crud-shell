3ceddb725e2be0d41a0b3f131618f4a1473115b7
Levy Chemelil <levy.chelal@student.moringaschool.com>
2024-10-17 13:34:03 +0300

    Update

--- server/app.py
+++ server/app.py
@@ -1,25 +1,76 @@
-# server/app.py
+#!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
-from models import db
+from models import db, Plant
 
-# create a Flask application instance 
 app = Flask(__name__)
-
-# configure the database connection to the local file app.db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# configure flag to disable modification tracking and use less memory
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-# create a Migrate object to manage schema modifications
 migrate = Migrate(app, db)
-
-# initialize the Flask application to use the database
 db.init_app(app)
 
+api = Api(app)
+
+class Plants(Resource):
+
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
+
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+
+api.add_resource(Plants, '/plants')
+
+class PlantByID(Resource):
+
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+        
+        db.session.commit()
+        
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
